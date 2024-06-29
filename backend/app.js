d5b3999e87f0e8668ee7128c46859d934df64395
ThibautCharpentier <thibaut.charpentier42@gmail.com>
2024-06-29 19:51:02 +0200

    feat: back - config bdd

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const db = require('./db/db');
+
+dotenv.config();
+
 const app = express();
-const port = 8000;
+const port =  process.env.BACK_PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+db.createTables().catch(err => {
+	console.log('Error creating tables: ', err);
+	exitServer();
 });
+
+const server = app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
+
+const exitServer = () => {
+	console.log('Shutting down server');
+	server.close(async () => {
+		await db.closePool();
+		process.exit(1);
+	})
+}
