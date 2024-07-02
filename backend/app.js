ce6fc02469b9030860b1ffe7414794ac15d1e207
ThibautCharpentier <thibaut.charpentier42@gmail.com>
2024-07-02 15:44:49 +0200

    feat: back - config ip/cors

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const db = require('./db/db');
+const authRouter = require('./routes/auth')
 
 dotenv.config();
 
 const app = express();
-const port =  process.env.BACK_PORT;
+const back_port = process.env.BACK_PORT;
+const front_port = process.env.FRONT_PORT;
+const url1 = process.env.LOCAL_IP;
+const url2 = process.env.LOCAL_IP2;
+const url3 = process.env.HOST_IP;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+app.use(cors({
+	origin: [`http://${url1}:${front_port}`, `http://${url2}:${front_port}`, `http://${url3}:${front_port}`],
+	methods: ['GET', 'POST'],
+	credentials: true
+}));
+
+app.use('/auth', authRouter);
 
 db.createTables().catch(err => {
 	console.log('Error creating tables: ', err);
 	exitServer();
 });
 
-const server = app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+const server = app.listen(back_port, '0.0.0.0', () => {
+	console.log(`Server is running on http://${url1}:${back_port}, http://${url2}:${back_port} and http://${url3}:${back_port}`);
 });
 
 const exitServer = () => {
