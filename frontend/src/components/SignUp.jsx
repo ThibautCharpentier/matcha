7e7b1b91f06331936980ba3cfde1d06398bd0630
Ismerie <ismerie.george@gmail.com>
2024-07-15 12:27:54 +0200

    feat: front style signup

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom"
+import { APP_ROUTES } from "../utils/constants"
+
 export default function SignUp() {
   return (
-    <div>
-        <div>
-          <h1>Créer un compte</h1>
-          <p>Vous avez déjà un compte ? <span>Se connecter</span></p>
-          <form action="">
+    <div className="flex justify-center">
+        <div className="w-80 flex flex-col bg-red-400 p-12 mt-12">
+          <h1 className="text-5xl font-poppins-bold">Créer un compte</h1>
+          <p className="font-poppins-regular">Vous avez déjà un compte ? <Link className="" to={ APP_ROUTES.SIGN_IN } >Se connecter</Link></p>
+          <form action="" className="flex flex-col">
             <label htmlFor="username">Nom d'utilisateur</label>
             <input type="text" name="username" id="username" />
             <label htmlFor="name">Prénom</label>
@@ -18,6 +21,7 @@ export default function SignUp() {
             <label htmlFor="confirmpassword">Confirmer mot de passe</label>
             <input type="text" name="confirmpassword" id="confirmpassword" />
           </form>
+          <button className="btn mt-6">Créer un compte</button>
         </div>
     </div>
   )
