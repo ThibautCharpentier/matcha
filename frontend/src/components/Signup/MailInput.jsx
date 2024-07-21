d7ad44227d358a777b5299662a2cdb7f71e0845a
Ismerie <ismerie.george@gmail.com>
2024-07-21 14:56:40 +0200

    fix: front - Signup errors return form

--- frontend/src/components/Signup/MailInput.jsx
+++ frontend/src/components/Signup/MailInput.jsx
@@ -1,16 +1,8 @@
 export default function MailInput({inputsStates, setInputsStates, showValidation}) {
-	let contentError = ""
-
-	if (showValidation.mail) {
-		if (inputsStates.mail.length === 0)
-			contentError = "Ce champ ne peut pas être vide"
-		else
-			contentError = "Ce mail est déjà utilisé"
-	}
 	
 	return (
 		<>
-			<label className="mt-3 font-poppins-medium" htmlFor="mail">Mail</label>
+			<label className="mt-3 font-poppins-medium" htmlFor="mail">Adresse e-mail</label>
 			<input
 				className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
 				type="text"
@@ -21,9 +13,9 @@ export default function MailInput({inputsStates, setInputsStates, showValidation
 				value={inputsStates.mail}
 				onChange={e => setInputsStates({...inputsStates, mail: e.target.value})}
 			/>
-			{showValidation.mail && (
-			<p className=" text-red-600 text-sm ">{contentError}</p>
-		)}
+			{showValidation.mail != "" && (
+			<p className=" text-red-600 text-sm ">{showValidation.mail}</p>
+			)}
 		</>
 	)
 }
