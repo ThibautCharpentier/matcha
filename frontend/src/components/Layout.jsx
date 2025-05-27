94bd63b0571f1baede1164a9bdbeb06b5df46e23
Ismerie George <ismerie.george@gmail.com>
2025-05-27 11:49:46 +0200

    style: color pop up notif

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,6 +5,12 @@ import { ToastContainer } from 'react-toastify';
 export default function Layout({ children }) {
 	const { isAuthenticated } = useAuth();
 
+	const contextClass = {
+		success: "bg-[--color-dark-green]",
+		error: "bg-[--color-pink]",
+		default: "bg-[--color-dark-green]",
+	};
+
 	return (
 		<>
 			{isAuthenticated ?
@@ -14,14 +20,18 @@ export default function Layout({ children }) {
 				/>
 				<ToastContainer
 					position="top-right"
-					autoClose={5000}
+					// autoClose={5000}
 					hideProgressBar={false}
 					closeOnClick
 					pauseOnHover
 					draggable
 					theme="colored"
+					toastClassName={(context) =>
+						contextClass[context?.type || "default"] +
+						" relative flex p-3 max-w-72 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
+					}
 				/>
-			</>	
+			</>
 			:
 			<>
 				<header></header>
